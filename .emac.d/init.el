0e1a5f8c11b6492295d232140de0f7546834aafb
electricRGB <github@acmelabs.ca>
2021-02-26 11:13:44 -0500

    More tweaking

--- .emac.d/init.el
+++ .emac.d/init.el
@@ -4,16 +4,18 @@
   (menu-bar-mode -1)
   (tool-bar-mode -1)
   (scroll-bar-mode -1)
-  (tooltip-mode -1))
+  (tooltip-mode -1)
+  (set-fringe-mode 10))
 
+(setq visible-bell t)
 (setq inhibit-startup-message t)
 (setq initial-scratch-message "")
 
 ;;; Set up package
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("elpa" . "https://elpa.gnu.org/packages/") t)
 (when (boundp 'package-pinned-packages)
   (setq package-pinned-packages
         '((org-plus-contrib . "org"))))
@@ -33,6 +35,7 @@
   (require 'use-package))
 (require 'diminish)                ;; if you use :diminish
 (require 'bind-key)
+(setq use-package-always-ensure t)
 
 ;;; Load the config
 (org-babel-load-file (concat user-emacs-directory "SpaceMacx.org"))
